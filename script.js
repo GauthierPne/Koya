78cf63e22f5737623987193bbd4824ac6e0c2c37
GauthierPne <gauthier2penne@gmail.com>
2025-08-26 22:51:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-// Sélectionne le bouton et le menu
-const toggleBtn = document.querySelector(".menu-toggle");
-const menu = document.querySelector(".menu");
 
-// Ajoute un clic pour ouvrir/fermer
-toggleBtn.addEventListener("click", () => {
-    menu.classList.toggle("active");
-});
+
